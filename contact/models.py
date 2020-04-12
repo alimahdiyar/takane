4bac6c4de6cd497f83e9b5e5baa355dae237ed58
yasinrezaei <yasinrezaei@hotmail.com>
2020-04-12 16:27:25 +0430

    log in  and register done

--- contact/models.py
+++ contact/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import User,auth
 class contact(models.Model):
+    site_owner = models.OneToOneField(User, on_delete=models.CASCADE, )
     first_name=models.CharField(max_length=20)
     last_name=models.CharField(max_length=20)
     email=models.EmailField()
